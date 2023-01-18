a14c1a426b0056751c691094e9ba94bcc72d0f52
Alyssa Padayhag <alyssapadayhag@gmail.com>
2023-01-18 12:51:22 -0500

    added routing

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,12 @@
 import Header from './Header';
 import Footer from './Footer';
+import Routing from './Routing';
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Header />
-      {children}
+      <Routing />
       {/* <Footer /> */}
     </div>
   );
