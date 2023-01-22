cf6511fdc29b91906dc760116f91257da67db0c2
bim <bimbulle9333@gmail.com>
2023-01-21 21:19:34 -0500

    added profile_atch logic and view accordian

--- src/components/ListJobs.js
+++ src/components/ListJobs.js
@@ -3,10 +3,14 @@ import { useState, useEffect } from 'react';
 import styles from '../styles/ListJobs.module.css';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { HiOutlineBookmark } from 'react-icons/hi'
+
+import { IconContext } from "react-icons";
 
 function ListJobs() {
 
     const [jobs, setJobs] = useState([]);
+    const [isActive, setIsActive] = useState(false);
 
     const fetchJobs=()=>{
       fetch('/jobData.json'
@@ -33,6 +37,7 @@ function ListJobs() {
     const jobList = jobs.map((job) => {
         return (
             <div className={styles.card}>
+            <div className={styles.carddisplay}>
                 <div className={styles.left}>
                     <h2>{job.title}</h2>
                     <h4>{job.company_name}</h4>
@@ -47,18 +52,64 @@ function ListJobs() {
                 </div>
                 <div className={styles.right}>
                     <div className={styles.progress}>
-                    <h4 className={styles.progressbar}><CircularProgressbar value={90} text={`${90}%`} /> </h4>
+                    <h4 className={styles.progressbar}>
+
+                        { job.profile_match >= 90 ? <CircularProgressbar value={job.profile_match} text={`${job.profile_match}%`} styles={{ path: { stroke: 'green' }}}/> :
+                            job.profile_match >= 70 ? <CircularProgressbar value={job.profile_match} text={`${job.profile_match}%`} styles={{ path: { stroke: 'yellow' }}}/> :
+                            <CircularProgressbar value={job.profile_match} text={`${job.profile_match}%`} styles={{ path: { stroke: 'red' }}}/>
+                        }
+                    </h4>
                         <h4 className={styles.match}> Match</h4>
                     </div>
                     <div className={styles.time}>
-                        <h6 className={styles.posted}>Posted</h6>
-                        <h5 className={styles.hours}>{job.posted} hrs ago</h5>
+                        <h6>Posted</h6>
+                        <h5>{job.posted} hrs ago</h5>
+                        <div className={styles.bottomright}>
+                        <IconContext.Provider value={{ size: '2.5em' }}>
+                            <HiOutlineBookmark/>
+                        </IconContext.Provider>
+                            <h4 className={styles.easyapply}>Easy Apply</h4>
+                        </div>
                     </div>
                 </div>
-                
+
+
 
             </div>
 
+                <div className="accordion">
+                    <div className="accordion-item">
+                        
+                    {isActive && <div className={styles.content}>
+                            
+                            <h4>Company Profile</h4>
+                            <p>{job.company_profile}</p>
+                            
+                            <h4>Required Professional Skills</h4>
+                            {job.required_pro_skills}
+
+                            <h4>Required Technical Skills</h4>
+                            {job.required_tech_skills}
+
+                            <p>Benefits:</p>
+                            {job.benefits}
+                            
+                            
+                            
+                            </div>}
+                        <div
+                        className="accordion-title"
+                        onClick={() => setIsActive(!isActive)}
+                        >
+                        {/* <div>{job.title}</div> */}
+                        <div>{isActive ? 'View -' : 'View'}</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+    
 
         )
     });
